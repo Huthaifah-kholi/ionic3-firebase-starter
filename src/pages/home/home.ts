0c4d79d2b6d81e0264be9c43831876b235e2949e
Huthaifah <huthaifah@huthaifahs-mbp.mynet>
2019-03-11 03:38:16 +0200

    add google map geolocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,17 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Geolocation, GeolocationOptions, Geoposition } from '@ionic-native/geolocation/ngx';
-
 import { trigger, transition, style, animate, state } from '@angular/animations';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -32,7 +41,7 @@ declare var google;
 })
 export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
-  map: any;
+  // map: any;
   currentMapTrack = null;
  
   isTracking = false;
@@ -41,12 +50,13 @@ export class HomePage {
   userData = {
     displayName: 'Stranger'
   };
-
+  // google
+  map: GoogleMap;
 
   greeting = "Hello";
   
-  options : GeolocationOptions;
-  currentPos : Geoposition;
+  // options : GeolocationOptions;
+  // currentPos : Geoposition;
 
   constructor(public navCtrl: NavController, public afAuth: AngularFireAuth,
     public toastCtrl: ToastController,private geolocation: Geolocation) {
@@ -63,29 +73,72 @@ export class HomePage {
 
   ionViewDidEnter(){
     //Set latitude and longitude of some place
-    this.map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.9011, lng: -56.1645 },
-      zoom: 15
-    });
-    this.getUserPosition();
-
-  
+    // this.getUserPosition();
+    
+        // This code is necessary for browser
+        Environment.setEnv({
+          'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+          'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+        });
+    
+        let mapOptions: GoogleMapOptions = {
+          camera: {
+             target: {
+               lat: 43.0741904,
+               lng: -89.3809802
+             },
+             zoom: 18,
+             tilt: 30
+           }
+        };
+    
+        this.map = GoogleMaps.create('map_canvas', mapOptions);
+    
+        let marker: Marker = this.map.addMarkerSync({
+          title: 'Ionic',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: 43.0741904,
+            lng: -89.3809802
+          }
+        });
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+          alert('clicked');
+        });
+      
   }
+
   getUserPosition(){
-    this.options = {
-        enableHighAccuracy : true
-    };
-
-    this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
-
-        this.currentPos = pos;      
-        console.log(pos);
-
-    },(err : PositionError)=>{
-        console.log("error : " + err.message);
-    });
+    console.log("getUserPosition() called");
+    // let _this=this;
+    // let locationOptions = { timeout: 10000, enableHighAccuracy: true };
+      if(navigator.geolocation) {
+        console.log("navigator.geolocation is available");
+        navigator.geolocation.getCurrentPosition(function(position) {
+          console.log("current position acquired",position);
+          // this.viewMap(position.coords.latitude, position.coords.longitude);
+          this.map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: position.coords.latitude, lng: position.coords.longitude},
+            zoom: 20
+          });
+          this.createToast("potion :"+position);
+        },function(error) {
+          console.log("error ",error);
+          
+        },{
+          enableHighAccuracy: true 
+      }
+        )  
+      }
+    
 }
-
+viewMap(lat,lng){
+  this.map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: lat, lng: lng},
+    zoom: 10
+  });
+}
   createToast(message: string) {
     return this.toastCtrl.create({
       message,
